0f18923b3889603a8edb800ef7de76819693c5b8
Shubham <shubhamgupto@gmail.com>
2020-09-20 08:32:52 +0530

    Updated A2 template

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -1,3 +1,26 @@
+
+def A_star_Traversal(
+    #add your parameters 
+):
+    l = []
+
+    return l
+
+def UCS_Traversal(
+    #add your parameters 
+):
+    l = []
+
+    return l
+
+def DFS_Traversal(
+    #add your parameters 
+):
+    l = []
+
+    return l
+
+
 '''
 Function tri_traversal - performs DFS, UCS and A* traversals and returns the path for each of these traversals 
 
@@ -16,40 +39,29 @@ start_point - single start node
 goals - list of size 'm' containing 'm' goals to reach from start_point
 
 Return : A list containing a list of all traversals [[],[],[]]
+1<=m<=n
+cost[n][n] , heuristic[n][n], start_point, goals[m]
 
 NOTE : you are allowed to write other helper functions that you can call in the given fucntion
 '''
 
-DFS_flag = 0
-UCS_dict = {}
-def DFS_recursive(n,cost,v,visited,goals):
-    global DFS_flag
-    if v not in visited and DFS_flag == 0:
-        visited.append(v)
-        if v in goals:
-            DFS_flag = 1
-            return
-
-        for i in range(1,n):
-            if DFS_flag != 0:
-                return
-            elif cost[v][i] != -1 and cost[v][i] != 0:
-                DFS_recursive(n,cost,i,visited,goals)
-            
-def DFS_Traversal(cost,start_point,goals):
-    n = len(cost)
-    visited = []
-    DFS_recursive(n,cost,start_point,visited,goals)
-    return visited
-
-def tri_Traversal(cost, heuristic, start_point, goals):
+def tri_traversal(cost, heuristic, start_point, goals):
+    n = len(cost[0]) - 1
+    m = len(start_point)
     l = []
-    # t1 <= DFS_Traversal
-    # t2 <= UCS_Traversal
-    # t3 <= A_star_Traversal
-    #t1 = DFS_Traversal(cost,start_point,goals)
-    t2 = UCS_Traversal(cost,start_point,goals)
-    #l.append(t1)
+
+    t1 = DFS_Traversal(
+    #send whatever parameters you require 
+)
+    t2 = UCS_Traversal(
+    #send whatever parameters you require 
+)
+    t3 = A_star_Traversal(
+    #send whatever parameters you require 
+)
+
+    l.append(t1)
     l.append(t2)
-    #l.append(t3)
+    l.append(t3)
     return l
+
