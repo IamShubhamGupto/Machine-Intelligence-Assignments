3c784e8a76311ff7cc81f8c62d3ef05bbb7afdcb
Shubham <shubhamgupto@gmail.com>
2020-09-21 09:20:31 +0530

    UCS done completely

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -2,22 +2,43 @@
 from queue import Queue, PriorityQueue
 DEBUG = 0
 def A_star_Traversal(
-    #add your parameters 
+    initial_state, final_states, cost, heuristic
 ):
     l = []
 
     return l
-def UCS_Traversal(start_point, cost, goals):   
-    return []
-def DFS_Traversal(initial_state,final_states,cost):
+
+def UCS_Traversal(
+   initial_state,final_states,cost
+):
+    path = []
+    queue = PriorityQueue()
+    visited = set()
+    queue.put((0, initial_state, [initial_state]))
+    while not queue.empty():
+        path_cost, node, path = queue.get()
+        visited.add(node)
+        if node in final_states:
+            break       
+        children = cost[node]
+        for child in range(1, len(children)):
+            if child not in visited and (children[child] > 0):
+                    queue.put((path_cost + children[child], child, path + [child]))
+        if DEBUG:
+            print("[UCS] Current path cost ",path_cost)
+            print("[UCS] Current node ",node)
+            print("[UCS] Path to current node ",path) 
+
+    return path
+
+def DFS_Traversal(
+    initial_state,final_states,cost
+):
     path = []
     stack = [initial_state]
     visited = set()
 
     while len(stack):
-        #print(lexicographical_vertex)
-        #min_vertex = min(min(stack), lexicographical_vertex)    
-        #print(min_vertex)
         current_node = stack.pop()
         if current_node not in visited:
             visited.add(current_node)
@@ -27,10 +48,9 @@ def DFS_Traversal(initial_state,final_states,cost):
             break
         no_neighbour = 1
         for neighbour in range(len(cost)-1,0,-1):
-            #print(current_node)
             if neighbour not in visited and cost[current_node][neighbour] > 0:
                 if DEBUG:
-                    print("Being added to stack = ",neighbour)  
+                    print("[DFS] Being added to stack = ",neighbour)  
                 stack.append(neighbour)
                 no_neighbour = 0
         if no_neighbour:
@@ -38,10 +58,11 @@ def DFS_Traversal(initial_state,final_states,cost):
             children = [i for i in range(len(cost)) if i not in visited and cost[stack[-1]][i] > 0]
             if len(children) > 0:
                 path.append(stack[-1])
-        #print(stack)        
+        if DEBUG:        
+            print("[DFS] Current value of stack ",stack)        
                 
-    #if DEBUG:
-    print(path)            
+    if DEBUG:
+        print("[DFS] ",path)            
     return path
 '''
 Function tri_traversal - performs DFS, UCS and A* traversals and returns the path for each of these traversals 
@@ -70,28 +91,13 @@ NOTE : you are allowed to write other helper functions that you can call in the 
 def tri_traversal(cost, heuristic, start_point, goals):
 
     l = []
+        
+    t1 = DFS_Traversal(start_point, goals, cost)
     
-    adjMat = [[0 for i in range(len(cost[0]))] for j in range(len(cost))]
-    for i in range(0,len(cost)):
-        for j in range(len(cost[0])):
-            adjMat[i][j] = 1 if cost[i][j] > 0 else 0
-    #for ele in adjMat:
-        #print(ele)  
-    '''
-    adjList = dict()
-    for i in range(len(cost)):
-        adjList[i] = [ j for j in range(len(cost[i])) if cost[i][j] > 0]
-    print(adjList)  
-    ''' 
-    
-    t1 = DFS_Traversal(start_point,goals,adjMat)
-    
-    t2 = UCS_Traversal(start_point, cost, goals)
+    t2 = UCS_Traversal(start_point, goals, cost)
 
-    t3 = A_star_Traversal(
-    #send whatever parameters you require 
-)
-    #print(t2)
+    t3 = A_star_Traversal(start_point, goals, cost, heuristic)
+
     l.append(t1)
     l.append(t2)
     l.append(t3)
--- Assignment2/sample_test_case.py
+++ Assignment2/sample_test_case.py
@@ -30,6 +30,11 @@ def test_case():
     #except:
         #print("SAMPLE TEST CASE 1 FOR THE DFS_TRAVERSAL FAILED")
     #try:
+    if (tri_traversal(cost,heuristic, 4, [6, 7, 10]))[0] == [4,1,2,6]:
+        print("SAMPLE TEST CASE 4 FOR THE  DFS_TRAVERSAL PASSED")
+    else:
+        print("SAMPLE TEST CASE 4 FOR THE  DFS_TRAVERSAL FAILED")
+
     if (tri_traversal(cost,heuristic, 1, [6, 7, 10]))[1] == [1, 5, 4, 7]:
         print("SAMPLE TEST CASE 2 FOR THE  UCS_TRAVERSAL PASSED")
     else:
